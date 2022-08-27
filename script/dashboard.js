9ee845876c5bd9f9b4ac4dcf68f41826a1f1e7e3
Timimah <timimah0117@gmail.com>
2022-08-27 13:38:03 +0100

    created script folder

---
+++ script/dashboard.js
@@ -0,0 +1,11 @@
+var myNav = document.getElementById('navBar')
+window.onscroll = () =>{
+    if(window.scrollY > 20){
+        myNav.classList.add('navbar-colored')
+        // myNav.classList.remove('navbar')
+    }
+    else{
+        // myNav.classList.add('navbar')
+        myNav.classList.remove('navbar-colored')
+    }
+}
