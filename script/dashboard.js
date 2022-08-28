d9d8572d7e7c3647fb44de8afbbca4840e7450bc
Timimah <timimah0117@gmail.com>
2022-08-28 12:56:36 +0100

    changed background color

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,13 +1,13 @@
-var myNav = document.getElementById('navBar')
-window.onscroll = () =>{
-    if(window.scrollY > 20){
-        myNav.classList.add('navbar-colored')
-        // myNav.classList.remove('navbar')
-    }
-    else{
-        // myNav.classList.add('navbar')
-        myNav.classList.remove('navbar-colored')
-    }
-}
+// var myNav = document.getElementById('navBar')
+// window.onscroll = () =>{
+//     if(window.scrollY > 200){
+//         myNav.classList.add('navbar-colored')
+//         // myNav.classList.remove('navbar')
+//     }
+//     else{
+//         // myNav.classList.add('navbar')
+//         myNav.classList.remove('navbar-colored')
+//     }
+// }
 let userBalance = document.getElementById('balance').innerText
 balance.innerText = userBalance
