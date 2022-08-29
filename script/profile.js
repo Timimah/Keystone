7917713acb4cd93e4a03d3dddfa478813a95c4e0
Timimah <timimah0117@gmail.com>
2022-08-29 13:05:18 +0100

    made a few changes

--- script/profile.js
+++ script/profile.js
@@ -8,19 +8,59 @@ editProfile = ()=>{
     edit_Details.style.display = "block"
 }
 editedProfile = ()=>{
-        userName =userName.value
+    if(userName.value == ""){
+        alert("Empty")
+        userName = document.getElementById("user_name").innerText
         document.getElementById("user_name").innerText = userName
+    }
+    else{
+        userName = userName.value
+        document.getElementById("user_name").innerText = userName
+    }
+    if(userAddress.value == ""){
+        alert("u empty")
+        userAddress = document.getElementById("user_address").innerText
+        document.getElementById("user_address").innerText = userAddress
+    }
+    else{
         userAddress = userAddress.value
         document.getElementById("user_address").innerText = userAddress
+    }
+    if(userNumber.value == ""){
+        alert("v empty")
+        userNumber = document.getElementById("user_number").innerText
+        document.getElementById("user_number").innerText = userNumber
+    }
+    else{
         userNumber = userNumber.value
         document.getElementById("user_number").innerText = userNumber
+    }
+    if(userNok.value == ""){
+        alert("Nok empty")
+        userNok = document.getElementById("user_nok").innerText
+        document.getElementById("user_nok").innerText = userNok
+    }
+    else{
         userNok = userNok.value
         document.getElementById("user_nok").innerText = userNok
+    }
+    if(userNokRel1.value == ""){
+        alert("Nokrel1 empty")
+        userNokRel1 = document.getElementById("user_nokrel1").innerText
+        document.getElementById("user_nokrel1").innerText = userNokRel1
+    }
+    else{
         userNokRel1 = userNokRel1.value
-        document.getElementById("user_nokrel").innerText = userNokRel1
+        document.getElementById("user_nokrel1").innerText = userNokRel1
+    }    
     update_profile_success.style.display = "flex"
     user_profile.style.display = "block"
     edit_Details.style.display = "none"
+    userName.value = ""
+    userAddress.value = ""
+    userNumber.value = ""
+    userNok.value = ""
+    userNokRel1.value = ""
 }
 closeDiv = ()=>{
     update_profile_success.style.display = "none"
