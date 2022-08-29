c846d02650369f9ba92f6c648d6f998641d40c4c
Timimah <timimah0117@gmail.com>
2022-08-29 13:05:03 +0100

    user can now sign up

--- script/signup.js
+++ script/signup.js
@@ -1,18 +1,36 @@
-let password_Profile = JSON.parse(localStorage.getItem("localpasswordProfile"))
-var passwordProfile =[]
-function Submit(){
-var  profile ={
- Phonenumber:phoneNumber.value,
-userpassword:userPassword.value,
-Confirmpassword:confirmPassword.value,
+let profile
+let signupProfile =[]
+if(localStorage.user_profile){
+  signupProfile = JSON.parse(localStorage.getItem("user_profile"))
 }
+let nextOofKin = document.getElementById("nok_rel")
+let selectedNok 
+submitDetails = () =>{
+  selectedNok = nextOofKin.options[nextOofKin.selectedIndex].text
+  profile = {
+    FirstName: first_name.value,
+    MiddleName: middle_name.value,
+    LastName: last_name.value,
+    userAddress: user_address.value,
+    NextofKin: nok.value,
+    NextofKinRel: selectedNok,
+    Phonenumber:phoneNumber.value,
+    userpassword:userPassword.value,
+}
+signupProfile.push(profile)
+
+localStorage.setItem("user_profile",JSON.stringify(signupProfile))
+alert("You have successfully signed up")
+first_name.value = ""
+middle_name.value = ""
+last_name.value = ""
+user_address.value = ""
+nok.value = ""
+selected_nok = ""
 phoneNumber.value =""
 userPassword.value =""
-confirmPassword.value =""
-passwordProfile.push(profile)
 
-localStorage.setItem("localpasswordProfile",JSON.stringify(password_Profile))
-console.log(passwordProfile)
+console.log(signupProfile)
 }
 
 var  allCode =[]
