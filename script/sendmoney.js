20a1c296873a077435d8a92bc75b8bdbdf9f3c02
Herculestech <ajalaisaacpromise18@gmail.com>
2022-09-07 07:22:06 -0500

    Fixed alot of bugs

--- script/sendmoney.js
+++ script/sendmoney.js
@@ -1,33 +1,87 @@
-let _amount = document.getElementById("amount")
-let select_bank = document.getElementById("select-bank")
-let selected_value = select_bank.options[select_bank.selectedIndex].text
-let acct_number = document.getElementById("acct-number")
-let _remark = document.getElementById("remark")
-let balance = 1000
-
-sendMoney = ()=>{
-    _remark = document.getElementById("remark")
-    if(acct_number.value == "" || _amount.value == "" || _remark.value == ""){
-        acct_number.placeholder += " required"
-        _amount.placeholder += " required"
-        _remark.placeholder += " required"
+if(localStorage.user_profile){
+    signupProfile = JSON.parse(localStorage.getItem("user_profile"))
+  }
+  if(localStorage.signedin_user){
+    signedInUser = JSON.parse(localStorage.getItem("signedin_user"))
+}
+let amountEntered =""
+let date2 = new Date()
+let currentTime2 = date2.toLocaleString()
+transferMessage.style.display="none"
+pinvalidation.style.display="none"
+function sendMoney(){
+    amountEntered=amount.value
+    userAcctNo=acctNumber.value
+    userRemark=remark.value
+    signedInUser.map((item) =>{
+        // updatedUserBalance = item.userBalance - parseFloat(amountEntered)
+         item.userBalance = updatedUserBalance
+        localStorage.setItem("signedin_user", JSON.stringify(signedInUser))
+    })
+    signupProfile.map((item) =>{
+        userAcctNo2 = item.accountNumber
+         item.accountNumber = userAcctNo2
+        localStorage.setItem("user_profile", JSON.stringify( signupProfile))
+    })
+    if (amountEntered==0){
+        amountEntered=""
+        userAcctNo=""
+        userRemark=""
+        alert("Enter a valid amount")
+    }else if (amountEntered==""){
+        amountEntered=""
+        userAcctNo=""
+        userRemark=""
+        alert("Enter a valid amount")
+    }else if (amountEntered>updatedUserBalance){
+        amountEntered=""
+        userAcctNo=""
+        userRemark=""
+        alert("insufficient balance")
+    }else if (userAcctNo!=userAcctNo2){
+        alert("Account number not found")
+    }else{
+        signedInUser.map((item) =>{
+            updatedUserBalance = item.userBalance - parseFloat(amountEntered)
+               updatedUserBalance = item.userBalance
+            localStorage.setItem("signedin_user", JSON.stringify(signedInUser))
+        })
+        signupProfile.map((item) =>{
+            userAcctNo2 = item.accountNumber
+             item.accountNumber = userAcctNo2
+            //  localStorage.setItem("user_profile", JSON.stringify( signupProfile))
+        })
+        transfer_money.style.display="none"
+        pinvalidation.style.display="block"
+    // console.log(amountEntered,userAcctNo,userRemark)
+    // alert(`You have successfully transferred $${amountEntered} to ${userAcctNo} on ${currentTime2}`)
+    disp_message.innerHTML =`<h3>You have successfully transferred $${amountEntered} to ${userAcctNo} on ${currentTime2}</h3><br><h3>Remark: ${userRemark}</h3>`
+        amountEntered=""
+        userAcctNo=""
+        userRemark=""
     }
-    if(_amount.value <= 0 || _amount.value > balance){
-        _amount.value = ""
-        _amount.placeholder = "Enter a valid amount"
+}
+function closeMessage(){
+    transfer_money.style.display="block"
+    transferMessage.style.display="none"
+    pinvalidation.style.display="none"
+}
+function enterPin(){
+    pinEntered=pinEntered.value
+    pinvalidation.style.display="block"
+    transfer_money.style.display="none"
+    signedInUser.map((item) =>{
+        updatedUserBalance = item.userBalance
+        userPin = item.profilePin
+    })
+    if (pinEntered!=userPin){
+        alert("incorrect pin")
+    }else if (pinEntered==0){
+        alert("Enter a valid pin")
+    }else if (pinEntered==""){
+        alert("Enter a valid pin")
+    }else{
+        pinvalidation.style.display="none"
+        transferMessage.style.display="block"
     }
-    else if(_amount.value < 50){
-        // alert("Amount should not be less than 50")
-        _amount.value = ""
-        _amount.placeholder = "The minimum amount is 50"
-    }
-    else if(!(acct_number.value == "" || _amount.value == "" || _remark.value == "")){
-        disp_message.innerHTML = `<h3>Your have successfully transfered $ ${_amount.value} to ${acct_number.value}</h3>`
-    }
-    
-    
-    console.log(_amount.value)
-    console.log(selected_value)
-    console.log(acct_number.value)
-    console.log(_remark.value)
 }
